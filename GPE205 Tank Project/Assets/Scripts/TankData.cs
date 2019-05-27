2824c782c9a36dd7ba671387155effb679aa3c24
Ethan Dean <ethdean@uat.edu>
2019-05-27 01:14:23 -0500

    Project 1 Milestone 1

---
+++ GPE205 Tank Project/Assets/Scripts/TankData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankData : MonoBehaviour
+{
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // list of the different components that other scripts can pull from
+    [Header("Comps")]
+    public Transform transform;
+    public TankManager mover;
+    public GameObject TankBullet1;
+
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // list of the different variables that other scripts can pull from
+    [Header("Vars")]
+    public float movementSpeed; 
+    public float reverseMovementSpeed;
+    public float roatationSpeed;
+    public float health = 2;
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // is called when the script instance is loaded
+    // tells what transform and mover do
+    private void Awake()
+    {
+        transform = GetComponent<Transform>();
+        mover = GetComponent<TankManager>();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ GPE205 Tank Project/Assets/Scripts/TankManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankManager : MonoBehaviour
+{
+    // allows this script to pull components and variables from TankData
+    public TankData data;
+    private CharacterController tankController;
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = GetComponent<TankData>();
+        tankController = GetComponent<CharacterController>();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    private void OnTriggerEnter()
+    {
+        data.health--;
+        if (data.health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    public void Movement (Vector3 movementDirection)
+    {
+        Vector3 moveDirection = data.transform.TransformDirection(movementDirection);
+
+        tankController.SimpleMove(moveDirection * data.movementSpeed);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    
+    public void Rotate (float direction)
+    {
+        data.transform.Rotate(new Vector3(0, direction * data.roatationSpeed * Time.deltaTime, 0));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    
+    
+}
+
+
+
