0a320f13900d578bc45cef4a82092472a1d4e640
Ethan Dean <ethdean@uat.edu>
2019-07-07 20:11:21 -0500

    Should be the most recent build

--- GPE205 Tank Project/Assets/Scripts/AI Scripts/Sight.cs
+++ GPE205 Tank Project/Assets/Scripts/AI Scripts/Sight.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Sight : MonoBehaviour
 {
-    public Transform player;
+    public GameObject player;
+    private Transform Playertf;
+    private Transform foundPlayer;
     public float maxAngle;
     public float maxRadius;
 
-    private bool isInFov = false;
+    private static bool isInFov = false;
 
+    private void Start()
+    {
+        foundPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        Playertf = player.GetComponent<Transform>(); //LMB
+    }
+
+    // Gizmos is what is responsible for drawing all of the lines that the AI uses to see
     private void OnDrawGizmos()
     {
+        // this sphere is responsible for setting the limits for the FOV and player detection
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, maxRadius);
 
+        // these lines set the FOV and can be changed by changing the value of maxAngle
         Vector3 fovLine1 = Quaternion.AngleAxis(maxAngle, transform.up) * transform.forward * maxRadius;
         Vector3 fovLine2 = Quaternion.AngleAxis(-maxAngle, transform.up) * transform.forward * maxRadius;
 
@@ -22,28 +33,37 @@ public class Sight : MonoBehaviour
         Gizmos.DrawRay(transform.position, fovLine1);
         Gizmos.DrawRay(transform.position, fovLine2);
 
+        // this if statement is used to check if the AI can see the player in its FOV
+        // Line changes from red to green when player is in its FOV
         if (!isInFov)
+        {
             Gizmos.color = Color.red;
-        else
-            Gizmos.color = Color.green;
-        
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, (player.position - transform.position).normalized * maxRadius);
+            Gizmos.DrawRay(transform.position, (player.transform.position - transform.position).normalized * maxRadius); //LMB
+        }
 
+        else
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(transform.position, (player.transform.position - transform.position).normalized * maxRadius);
+        }
+
+        // Shows the center of the FOV
         Gizmos.color = Color.black;
         Gizmos.DrawRay(transform.position, transform.forward * maxRadius);
-    }
 
+    }
+    // This is a list of checks that checks if the player is in the FOV
+    // If the player is in the FOV, it sets isInFov to true, if not, it sets it to false
     public static bool inFOV(Transform checkingObject, Transform target, float maxAngle, float maxRadius)
     {
         Collider[] overlaps = new Collider[10];
         int count = Physics.OverlapSphereNonAlloc(checkingObject.position, maxRadius, overlaps);
 
-        for (int i = 0; i < count + 1; i++)
+        for (int i = 0; i < count; i++)
         {
             if (overlaps[i] != null)
             {
-                if (overlaps[i].transform == target)
+                if (overlaps[i].transform == target) //LMB // Original = overlaps[i].gameObject.transform == target
                 {
                     Vector3 directionBetween = (target.position - checkingObject.position).normalized;
                     directionBetween.y *= 0;
@@ -59,9 +79,10 @@ public class Sight : MonoBehaviour
                         if (Physics.Raycast(ray, out hit, maxRadius))
                         {
                             if (hit.transform == target)
+                            {
+                                isInFov = true;
                                 return true;
-                            
-
+                            }
                         }
 
                     }
@@ -71,11 +92,24 @@ public class Sight : MonoBehaviour
 
             }
         }
+        isInFov = false;
         return false;
     }
 
+    // updates every frame
     private void Update()
     {
-        isInFov = inFOV(transform,player,maxAngle,maxRadius);
+        Playertf = player.GetComponent<Transform>();
+        isInFov = inFOV(transform, Playertf, maxAngle, maxRadius);
+  
     }
+
+    private void Attack()
+    {
+        if (isInFov == true)
+        {
+            
+        }
+    }
+
 }
