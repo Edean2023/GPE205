790ea62158613ee37783a81068265297fed160ac
KNOWN-UNIVERSE\ethdean <ethdean@uat.edu>
2019-08-12 00:54:07 -0700

    MainMenu and GameOver complete

---
+++ GPE205 WORKING/Assets/Scripts/Menus/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOver : MonoBehaviour
+{
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level Gen");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
